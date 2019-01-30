df69b79c82d1b076edecc9a62c9dd7827fd714b3
Freddie <freddie.runnding@yh.nackademin.se>
2019-01-30 16:55:35 +0100

    added a close animation that doesn't work yet

--- ASP.NET MVC 1/Inlämningsuppgifter/Blogg/wwwroot/js/main.js
+++ ASP.NET MVC 1/Inlämningsuppgifter/Blogg/wwwroot/js/main.js
@@ -4,6 +4,7 @@ var overlay = document.getElementById('overlay');
 var closeMenu = document.getElementById('close-menu');
 
 document.getElementById('open-menu').addEventListener('click', function () {
+    overlay.classList.remove('close-menu');
     overlay.classList.add('show-menu');
 });
 document.getElementById('close-menu').addEventListener('click', function () {
