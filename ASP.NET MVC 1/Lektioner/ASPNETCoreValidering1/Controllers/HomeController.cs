b332534522bf5f1f7864c3814d26465ee9f5a5be
Freddie <freddie.runnding@yh.nackademin.se>
2019-01-30 10:24:52 +0100

    added validation demo

---
+++ ASP.NET MVC 1/Lektioner/ASPNETCoreValidering1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNETCoreValidering1.Models;
+
+namespace ASPNETCoreIntro2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Person person)
+        {
+            // Server validation of the model
+            if (ModelState.IsValid)
+            {
+                // Save to database
+                return View();
+
+            }
+            else
+            {
+                // Back to form
+                return View();
+
+            }
+        }
+    }
+}
---
+++ ASP.NET MVC 1/Lektioner/ASPNETCoreValidering1/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreValidering1.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="Name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must contain between 2 and 100 characters")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage = "Enter correct email")]
+        [StringLength(50, ErrorMessage = "Email must contain max 50 characters")]
+        public string Email { get; set; }
+        
+
+    }
+}
