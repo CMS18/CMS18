16b3c4814bab11f36c34748a7f0c506b1c61f51a
Freddie <freddie.runnding@yh.nackademin.se>
2019-02-11 10:39:02 +0100

    AJAX!

---
+++ ASP.NET MVC 1/Lektioner/ASPNETCoreAJAX1/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreAJAX1.Models
+{
+    public class Product
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+        //public Product (int id, string name, int price, string description)
+        //{
+        //    ID = id;
+        //    Name = name;
+        //    Price = price;
+        //    Description = description;
+
+        //}
+    }
+}
